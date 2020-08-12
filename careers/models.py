7d60588380868474e69da20fd5d14970e8b39399
S-inTheory <livsi48@gmail.com>
2020-08-12 12:13:36 +0300

    added dataload file

--- careers/models.py
+++ careers/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-# from datetime import date
-
-# from careers import data
 
 
 class Company(models.Model):
@@ -39,21 +36,3 @@ class Application(models.Model):
     written_cover_letter = models.TextField()
     vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='applications')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='applications')
-
-# for title in data.companies:
-#     Company.objects.create(name=title['title'])
-#
-# for unit in data.specialties:
-#     Speciality.objects.create(code=unit['code'], title=unit['title'])
-#
-# for unit in data.jobs:
-#     unit_posted = unit['posted'].split('-')
-#     speciality = Speciality.objects.get(code=unit['cat'])
-#     company = Company.objects.get(name=unit['company'])
-#     Vacancy.objects.create(title=unit['title'], speciality=speciality,
-#                            company=company, salary_min=unit['salary_from'],
-#                            salary_max=unit['salary_to'],
-#                            published_at=date(int(unit_posted[0]),
-#                                              int(unit_posted[1]),
-#                                              int(unit_posted[2])),
-#                            description=unit['desc'])
